a2068cd0b598ee1ec8842d9e287c2c70d47202c0
ericelvendahl <ericelvendahl@gmail.com>
2020-07-20 20:22:52 -0500

    Revised solutions

--- joins-solution.sql
+++ joins-solution.sql
@@ -17,7 +17,10 @@ SELECT "order_id", "quantity", "description"
 SELECT "warehouse"."warehouse" 
 	FROM "warehouse" 
 		JOIN "warehouse_product" 
-			ON "warehouse"."id" = "warehouse_product"."warehouse_id" JOIN "products" ON "warehouse_product"."product_id" = "products"."id" WHERE "description" = 'cheetos';
+		ON "warehouse"."id" = "warehouse_product"."warehouse_id" 
+			JOIN "products" 
+			ON "warehouse_product"."product_id" = "products"."id" 
+				WHERE "description" = 'cheetos';
 
 -- 4. Which warehouses have diet pepsi?
 SELECT "warehouse"."warehouse" 
@@ -36,4 +39,24 @@ SELECT "first_name", "last_name", count("orders") AS "number_of_orders"
 			GROUP BY "customers"."id";
 
 -- 6. How many customers do we have?
-SELECT COUNT(*) FROM "customers";
+SELECT COUNT(*) 
+	FROM "customers";
+
+-- 7. How many products do we carry?
+SELECT COUNT(*) 
+	FROM "products";
+
+-- 8. What is the total available on-hand quantity of diet pepsi?
+SELECT sum("on_hand") 
+	AS "total_pepsi_on_hand" 
+		FROM "warehouse_product" 
+			JOIN "products" 
+			ON "warehouse_product"."product_id" = "products"."id" 
+				WHERE "description" = 'diet pepsi';
+
+-- 9. How much was the total cost for each order?
+-- SELECT sum(price) FROM "line_items" JOIN "products" ON "line_items"."product_id" = "products"."id" GROUP BY "order_id";
+
+-- 10. How much has each customer spent in total?
+
+-- 11. How much has each customer spent in total? Customers who have spent $0 should still show up in the table. It should say 0, not NULL (research coalesce).
